3fbbbeae70aeebfcec0f97d5d731afe8aa9cb4e1
zhangshangzhi <zhangshangzhi@bevol.cn>
2024-03-15 18:49:37 +0800

    Initial commit

---
+++ backend/ocr/ocr_demo.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+# @Time : 2024/3/15 11:55
+# @File : ocr_demo.py
+# @Author: zhangshangzhi
+# @Description:
+
+
+from paddleocr import PaddleOCR, draw_ocr
+
+# Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
+# 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
+ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
+img_path = 'source/looong.jpg'
+img_path = 'source/640_p1.jpg'
+result = ocr.ocr(img_path, cls=True)
+for idx in range(len(result)):
+    res = result[idx]
+    for line in res:
+        print(line)
+
+# 显示结果
+from PIL import Image
+result = result[0]
+image = Image.open(img_path).convert('RGB')
+boxes = [line[0] for line in result]
+txts = [line[1][0] for line in result]
+scores = [line[1][1] for line in result]
+im_show = draw_ocr(image, boxes, txts, scores, font_path='source/simfang.ttf')
+im_show = Image.fromarray(im_show)
+im_show.save('source/result.jpg')
